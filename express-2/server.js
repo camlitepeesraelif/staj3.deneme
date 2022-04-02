3a48f8559ff84e3d8a25ee361ce051176b17c1f1
camlitepeesraelif <99489375+camlitepeesraelif@users.noreply.github.com>
2022-04-02 14:10:12 +0300

    staj3.deneme

---
+++ express-2/server.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const app = express ()
+
+app.get('/', function (req,res){
+    res.send('hello world')
+})
+
+app.get('/merhaba', function (req,res){
+    res.send('sanada merhaba')
+})
+export class UserProvider extends Component {
+    state={
+        dispatch:action =>{
+            this.setState(state => recuder(state,action))
+        }
+    }
+}
+app.listen(3000 ,()=>{
+    console.log ('express app listening on port http://localhost:3000')
+})
